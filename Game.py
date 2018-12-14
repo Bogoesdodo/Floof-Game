6314ef63661804d9dff6a712e26c3979319549c4
Bogoesdodo <kuangkevin3188@gmail.com>
2018-12-14 10:01:23 -0800

    created suer basic outlune of code

--- Game.py
+++ Game.py
@@ -1,72 +1,88 @@
-#Sample Code
+#Starting Code
 
+
+import config
 import arcade
-
-# --- Set up the constants
-
-# Size of the screen
-SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 600
-
-# Size of the rectangle
-RECT_WIDTH = 50
-RECT_HEIGHT = 50
+import os
 
 
-def on_draw(delta_time):
-    """
-    Use this function to draw everything to the screen.
-    """
 
-    # Start the render. This must happen before any drawing
-    # commands. We do NOT need a stop render command.
-    arcade.start_render()
+SCREEN_WIDTH = 1150
+SCREEN_HEIGHT = 950
 
-    # Draw a rectangle.
-    # For a full list of colors see:
-    # http://arcade.academy/arcade.color.html
-    arcade.draw_rectangle_filled(on_draw.center_x, on_draw.center_y,
-                                 RECT_WIDTH, RECT_HEIGHT,
-                                 arcade.color.ALIZARIN_CRIMSON)
+MOVEMENT_SPEED = 5
 
-    # Modify rectangles position based on the delta
-    # vector. (Delta means change. You can also think
-    # of this as our speed and direction.)
-    on_draw.center_x += on_draw.delta_x * delta_time
-    on_draw.center_y += on_draw.delta_y * delta_time
 
-    # Figure out if we hit the edge and need to reverse.
-    if on_draw.center_x < RECT_WIDTH // 2 \
-            or on_draw.center_x > SCREEN_WIDTH - RECT_WIDTH // 2:
-        on_draw.delta_x *= -1
-    if on_draw.center_y < RECT_HEIGHT // 2 \
-            or on_draw.center_y > SCREEN_HEIGHT - RECT_HEIGHT // 2:
-        on_draw.delta_y *= -1
+class MyGame(arcade.Window):
+    """ Main application class. """
 
-# Below are function-specific variables. Before we use them
-# in our function, we need to give them initial values. Then
-# the values will persist between function calls.
-#
-# In other languages, we'd declare the variables as 'static' inside the
-# function to get that same functionality.
-#
-# Later on, we'll use 'classes' to track position and velocity for multiple
-# objects.
-on_draw.center_x = 100      # Initial x position
-on_draw.center_y = 50       # Initial y position
-on_draw.delta_x = 115  # Initial change in x
-on_draw.delta_y = 130  # Initial change in y
+    def __init__(self, width, height):
+
+        super().__init__(width, height)
+
+        # Set the working directory (where we expect to find files) to the same
+        # directory this .py file is in. You can leave this out of your own
+        # code, but it is needed to easily run the examples using "python -m"
+        # as mentioned at the top of this program.
+        file_path = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(file_path)
+
+        # Sprite lists
+
+
+    def setup(self):
+        """ Set up the game and initialize the variables. """
+
+        # initializing Sprite lists
+
+        # Set the background color
+        arcade.set_background_color(arcade.color.AMAZON)
+        self.view_left = 0
+        self.view_bottom = 0
+
+    def on_draw(self):
+        """
+        Render the screen.
+        """
+
+        # This command has to happen before we start drawing
+        arcade.start_render()
+
+        # Draw all the sprites.
+
+
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed. """
+
+        #if key == arcade.key.UP:
+           # self.player_sprite.change_y = MOVEMENT_SPEED
+        #elif key == arcade.key.DOWN:
+        #    self.player_sprite.change_y = -MOVEMENT_SPEED
+        #elif key == arcade.key.LEFT:
+         #   self.player_sprite.change_x = -MOVEMENT_SPEED
+        #elif key == arcade.key.RIGHT:
+            #self.player_sprite.change_x = MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key. """
+
+        #if key == arcade.key.UP or key == arcade.key.DOWN:
+        #    self.player_sprite.change_y = 0
+       # elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
+           # self.player_sprite.change_x = 0
+
+    def update(self, delta_time):
+        """ Movement and game logic """
+
+        # Call update on all sprites (The sprites don't do much in this
+        # example though.)
+        #self.physics_engine.update()
 
 
 def main():
-    # Open up our window
-    arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Bouncing Rectangle Example")
-    arcade.set_background_color(arcade.color.WHITE)
-
-    # Tell the computer to call the draw command at the specified interval.
-    arcade.schedule(on_draw, 1 / 80)
-
-    # Run the program
+    """ Main method """
+    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT)
+    window.setup()
     arcade.run()
 
 
