c55ae328d0175798344d5de0b5069c836db681c2
Bogoesdodo <kuangkevin3188@gmail.com>
2019-01-08 20:05:41 -0800

    text box with text

--- displayText.py
+++ displayText.py
@@ -7,15 +7,21 @@ class displayText(object):
     display = False
     def __init__(self, text):
         self.text = text
+        self.text_sprite = arcade.Sprite("textbox.png", 1)
+        self.text_sprite.center_x = 870
+        self.text_sprite.center_y = 70
 
     def spriteDeclare(self, picture, size, x, y):
-        self.text_list = arcade.SpriteList()
+
         self.text_sprite = arcade.Sprite(picture, size)
         self.text_sprite.center_x = x
         self.text_sprite.center_y = y
-        self.text_list.append(self.text_sprite)
+
 
     def showDisplay (self, display):
         self.display = display
+        self.text_list = arcade.SpriteList()
+        self.text_list.append(self.text_sprite)
         if display :
             self.text_list.draw()
+            arcade.draw_text(self.text, self.text_sprite.center_x - 16, self.text_sprite.center_y + 3, arcade.color.BLACK, 14)
